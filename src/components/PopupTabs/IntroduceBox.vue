0dbbfcec915a74402b61efd8680be9771fdf7085
xuan <xuancbblin@gmail.com>
2022-09-09 14:25:11 +0800

    feat : create PopupTabs

---
+++ src/components/PopupTabs/IntroduceBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="h-[325px]" ref="dom">
+    <ul class="flex">
+      <li
+        class="w-[50%] py-3 text-center cursor-pointer"
+        :class="currentName === introduce.name ? 'bg-dark-700 text-white' : ''"
+        v-for="introduce in introduceData"
+        :key="introduce"
+        @click="currentName = introduce.name"
+      >
+        {{ introduce.name }}
+      </li>
+    </ul>
+    <div v-for="introduce in introduceData" :key="introduce">
+      <template v-if="currentName === introduce.name">
+        <img :src="introduce.img" :alt="introduce.name" />
+        <p>{{ introduce.content }}</p>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const props = defineProps({
+  introduceData: Array,
+  currentName: String,
+});
+
+const emit = defineEmits(["getDom"]);
+
+const dom = ref(null);
+
+onMounted(() => {
+  emit("getDom", dom.value);
+});
+</script>
---
+++ src/components/PopupTabs/PopupTabs.vue
@@ -0,0 +1,30 @@
+<template>
+  <Map :dom="dom" />
+  <IntroduceBox :currentName="currentName" :introduceData="introduceData" @getDom="getDom" />
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+import IntroduceBox from "./IntroduceBox.vue";
+import Map from "./Map.vue";
+
+const dom = ref(null);
+const currentName = ref("tab");
+
+const introduceData = reactive([
+  {
+    name: "tab",
+    img: "https://picsum.photos/id/684/600/400",
+    content: "Ipsum ratione nulla iure rem tempora, quasi quis minima consequatur",
+  },
+  {
+    name: "tab2",
+    img: "https://picsum.photos/id/685/600/400",
+    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit",
+  },
+]);
+
+const getDom = (boxDom) => {
+  dom.value = boxDom;
+};
+</script>
